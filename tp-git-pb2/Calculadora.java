b5d6461f0d3455c338834ddd07a1461c78c98f7e
marianoperri <marianoperri@gmail.com>
2020-09-18 12:54:31 -0300

    modificacion comentario para nacho

--- tp-git-pb2/Calculadora.java
+++ tp-git-pb2/Calculadora.java
@@ -6,8 +6,7 @@ public class Ahorcado {
  Integer opcion;
  Integer resultado;
  do{
- System.out.println("Ingrese su operación /n 1 para sumar /n 2 para
-restar /n 3 para multiplicar /n 4 para dividir");
+ System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
  opcion = teclado.next();
  }while(opcion < 1 && opcion > 4)
  System.out.println("Ingrese el primer numero");
